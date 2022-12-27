cab5dbeab34b4bc362e841e6afa2abe86dc6bb5f
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-12-28 02:57:10 +0600

    CRUD operation added for blogs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -15,21 +15,86 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const skillsCollection = client.db('portfolio').collection('skills')
-        
+        const projectCollection = client.db('portfolio').collection('projects')
+        const blogsCollection = client.db('portfolio').collection('blogs')
+
         app.get('/skills', async (req, res) => {
             const skills = await skillsCollection.find({}).toArray()
             res.send(skills)
         })
 
-    } finally {}
+        // projects
+        app.get('/projects', async (req, res) => {
+            const projects = await projectCollection.find({}).toArray()
+            res.send(projects)
+        })
+
+        app.post('/projects', async (req, res) => {
+            const project = req.body
+            const result = await projectCollection.insertOne(project)
+            res.send(result)
+        })
+
+        // blogs
+        app.post('/blogs', async (req, res) => {
+            const blog = req.body
+            const result = await blogsCollection.insertOne(blog)
+            res.send(result)
+        })
+        app.get('/blogs', async (req, res) => {
+            const result = await blogsCollection.find({}).toArray()
+            res.send(result)
+        })
+        app.get('/blogs/:name', async (req, res) => {
+            const name = req.params.name
+            const query = { title: name }
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
+        app.put('/blogs/:id', async (req, res) => {
+            const id = req.params.id
+            const blog = req.body
+            const { title, body, tags, date } = blog
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            if (blog.img) {
+                const updatedDoc = {
+                    $set: {
+                        title, body, tags, date, img: blog.img
+                    }
+                }
+                const result = await blogsCollection.updateOne(filter, updatedDoc, options)
+                res.send(result)
+            }
+            else {
+                const updatedDoc = {
+                    $set: {
+                        title, body, tags, date
+                    }
+                }
+                const result = await blogsCollection.updateOne(filter, updatedDoc, options)
+                res.send(result)
+                
+            }
+        })
+        app.delete('/blogs/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await blogsCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+
+
+    } finally { }
 }
-run().catch(err=>console.error(err))
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
     res.send('Portfolio Server is running...')
 }),
 
-app.listen(port, () => {
-    console.log(`portfolio server is running on ${port}`)
-})
+    app.listen(port, () => {
+        console.log(`portfolio server is running on ${port}`)
+    })
